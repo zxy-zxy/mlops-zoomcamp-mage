a019bfe9fb54a2905009fb5bf795712a8874a6fa
ivan <sinitsinvanya@gmail.com>
2024-06-09 17:00:47 +0400

    Mage Homework

--- mlops/homework_03/transformers/build.py
+++ mlops/homework_03/transformers/build.py
@@ -11,33 +11,21 @@ def train_model(df: pd.DataFrame, **kwargs):
     Train a linear regression model using DictVectorizer on categorical features.
     Returns a dictionary containing the DictVectorizer and the trained model.
     """
-    # Define categorical columns
+
     categorical = ['PULocationID', 'DOLocationID']
-    
-    # Convert categorical features to dictionary format
     train_dicts = df[categorical].to_dict(orient='records')
     
-    # Apply DictVectorizer to transform categorical features into a feature matrix
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts)
     
-    # Define the target variable
     target = 'duration'
     y_train = df[target].values
     
-    # Train a linear regression model
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     
-    # Make predictions on the training set
     y_pred = lr.predict(X_train)
+
+    print(f'{lr.intercept_}')
     
-    # Calculate the Root Mean Squared Error (RMSE) on the training set
-    train_rmse = mean_squared_error(y_train, y_pred, squared=False)
-    print(f'Train RMSE: {train_rmse}')
-    
-    # Print the intercept of the model
-    print(f'Intercept of the model: {lr.intercept_}')
-    
-    # Return the DictVectorizer and the trained model
     return (lr, dv)
